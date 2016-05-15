dfd11dab4cf49bc348189656b3068a98317ca601
jjcoffman <jjcoffman@me.com>
2016-05-14 17:59:08 -0700

    Added the inception of code for Executing the software and unzipping software in a platform independent environment

---
+++ Gopher/src/Starter.java
@@ -0,0 +1,13 @@
+
+public class Starter 
+{
+
+	/**
+	 * Main creates the download directory for tools and runs each tool.
+	 * @author Jonathan Coffman
+	 */
+	public static void main(String[] args) 
+	{
+		//TODO
+	}
+}
